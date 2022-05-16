e505db2f4ffd3d17a5fb324d3c0416f5a8ab6a49
제동진 <43737240+tkroahdla@users.noreply.github.com>
2022-05-16 18:56:48 +0900

    Front RFID업데이트 -동진-
    Front RFID업데이트 -동진-

--- 1-TwoRC522_RPi2-3/card_reader.py
+++ 1-TwoRC522_RPi2-3/card_reader.py
@@ -6,6 +6,7 @@ Class in Python 2.7 that executes a Thread for reading RFID tags.
 
 """
 import requests
+import time
 import threading
 import signal
 import RPi.GPIO as GPIO
@@ -60,14 +61,9 @@ class Nfc522(object):
             # If we have the UID, continue
             if status == MIFAREReader.MI_OK:
                 
-                uid1 = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
-                URL = 'http://0.0.0.0:8082/frontRfid?uid='+uid1
-                print(uid1)
-                
-                response = requests.get(URL)
-                print "앞쪽 RFID 감지"
+               
+                print "Front RFID"
                 # Print UID
-                print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
                 
 
                 # GPIO.output(24,GPIO.HIGH)   # Code For Turn ON/OFF Buzzer
@@ -89,6 +85,26 @@ class Nfc522(object):
                     MIFAREReader.MFRC522_StopCrypto1()
                 else:
                     print "Authentication error"
+                    
+                print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+                uid1 = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
+                url3= "http://172.30.1.41:8081/myapp/frontRfid?uid="+uid1+"&umbbox_seq=1"
+                try : 
+                    requests.get(url3)
+                            #연결 시간초과 예외처리
+                except requests.exceptions.Timeout:
+                    pass
+
+                    #SSL 인증서 에러 발생 방지
+                except requests.exceptions.SSLError:
+                    pass
+            
+                    #연결 실패(응답 없음) 예외처리
+                except requests.exceptions.ConnectionError:
+                    pass
+                
+                sleep(0.1)
+
                 
 
 class CardReader(threading.Thread):
@@ -133,5 +149,6 @@ class CardReader(threading.Thread):
     #         print "I make interesting operations here with the tag:" + str(numero)
     #     except Exception as e:
     #         print e
+
 
 
